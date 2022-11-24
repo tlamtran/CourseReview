ce88f7c45b5490b2e8a6dc6ab795ab915fd2b044
Chaulilou <chaulilou@gmail.com>
2022-11-24 19:06:06 +0200

    Progress bar

--- src/components/ReviewsStats.js
+++ src/components/ReviewsStats.js
@@ -1,4 +1,5 @@
 import StarReview from "./StarReview"
+import { ProgressBar } from "baseui/progress-bar";
 
 const ReviewsStats = ({ reviews }) => {
   const score =
@@ -9,6 +10,28 @@ const ReviewsStats = ({ reviews }) => {
 
   return (
     <div className="reviews-stats"> 
+      <div>
+        Difficulty <ProgressBar
+          value = {((reviews.map((r) => r.difficulty).reduce((x, y) => x+y, 0))/reviews.length)*20}
+          overrides={{
+            BarProgress: {
+              style: {
+                backgroundColor: '#DCB757',
+              }
+            },
+            BarContainer : {
+              style: {
+                backgroundColor: '#D9D9D9',
+                outline: '#FFFFFF',
+                width: '40%',
+                position: 'relative',
+                top: '10%',
+                left: '10%'
+              }
+            }
+          }}
+        />
+      </div> 
       <h1 style={{ fontFamily: "Inter", position: 'relative', left: '65%', fontSize: '75px' }}>{Math.round(score * 10) / 10}</h1>
       <StarReview starValue={(Math.round(score * 10)/10)} starSize={30} starstyle={{ position: 'relative', left: '62.5%', bottom: 40 }}/>
     </div> 
